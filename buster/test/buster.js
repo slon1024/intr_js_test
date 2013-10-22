318020c1dbe99af2cf1ab26e86d445a78390a0ab
Vladimir Alekseichenko <vova@vova.me>
2013-10-22 06:46:48 +0200

    buster

---
+++ buster/test/buster.js
@@ -0,0 +1,11 @@
+var config = module.exports;
+
+config["My tests"] = {
+    environment: "node",        // or "browser"
+    rootPath: "../",
+    sources: [
+    ],
+    tests: [
+        "test/*Test.js"
+    ]
+};
